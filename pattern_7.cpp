fe099ca676c3a9fe6e99f420b50dd870a1215389
Niyati Belani <niyatibelani1020@gmail.com>
2026-02-02 23:00:26 +0530

    Add files via upload

---
+++ pattern_7.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for (int row = 1; row <= 5; row++)
+    {
+        /* code */
+        for (int col = 1; col <= row; col++)
+        {
+            /* code */
+            cout<<"* ";
+        }
+
+        cout<<endl;
+        
+    }
+    
+}
