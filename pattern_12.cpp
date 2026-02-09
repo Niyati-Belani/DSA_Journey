ad2277e341d308ae4453a9996d773f594c9be231
Niyati Belani <niyatibelani1020@gmail.com>
2026-02-09 21:47:42 +0530

    Add files via upload

---
+++ pattern_12.cpp
@@ -0,0 +1,38 @@
+
+//METHOD-1
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for (int row = 1; row <= 5 ; row++)
+    {
+        for (int col = 1; col <= 5-row+1; col++)
+        {
+            cout<<"* ";
+        }
+        
+        cout<<endl;
+    }
+    
+}
+
+//METHOD-2
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for (int row = 5; row >= 1 ; row--)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            cout<<"* ";
+        }
+        
+        cout<<endl;
+    }
+    
+}
