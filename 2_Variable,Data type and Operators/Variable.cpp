26b02b5ff159664fc559cbae2d6f4e487e488ff3
Niyati-Belani <niyatibelani1020@gmail.com>
2026-05-08 17:08:32 +0530

    Add Variable File

---
+++ 2_Variable,Data type and Operators/Variable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 19;
+    char grade = 'A';
+    float PI = 3.14;
+    bool isSafe = false;
+    double price = 100.99;
+
+    cout << "Age is : " << age << endl;
+    cout << "Grade is : " << grade << endl;
+    cout << "PI is : " << PI << endl;
+    cout << "isSfae =  " << isSafe << endl;
+    cout << "Price is : " << price << endl;
+    return 0;
+}
