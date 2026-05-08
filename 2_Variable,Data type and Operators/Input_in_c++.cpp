ea7f73213c71e2cc4a38d0a0fc0ccf4c3b7e194d
Niyati-Belani <niyatibelani1020@gmail.com>
2026-05-08 17:41:33 +0530

    Add Input File

---
+++ 2_Variable,Data type and Operators/Input_in_c++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int age;
+    cout << "Enter your Age: ";
+    cin >> age;
+
+    cout << "Your Age is: " << age << endl;
+    return 0;
+    
+}
