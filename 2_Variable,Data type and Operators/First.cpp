a152ab68f551467ca631460a779e6e8ade951e87
Niyati-Belani <niyatibelani1020@gmail.com>
2026-05-08 16:50:31 +0530

    Add Comments

--- 2_Variable,Data type and Operators/First.cpp
+++ 2_Variable,Data type and Operators/First.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 int main()
 {
-    cout << "Niyati DSA Journey.";
+    cout << "Niyati DSA Journey." << endl;
     return 0;
 }
 
+/*
+    Variable:-
+    -Containers to store data
+    -Variables values can be Vary 
+    -Name of the variable cann't vary
+
+    Rules:-
+    -Starts with Letters Like A-Z , a-z Or "_".
+    -Variable name should not same as keywords.
+
+    Example:-
+        int a=10
+        int b=20
+        int age=19
+        char grade=A
+*/
