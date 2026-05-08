1da515052acde97df09a156f3b5c7f59e40c756c
Niyati-Belani <niyatibelani1020@gmail.com>
2026-05-08 18:28:59 +0530

    Add new file 2_Relation into 5_Operators folder.

---
+++ 2_Variable,Data type and Operators/5_Operators/2_Relational.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << (3<5) << endl;
+    cout << (3>5) << endl;
+    cout << (3<=3) << endl;
+    cout << (3==3) << endl;
+    cout << (5==3) << endl;
+    cout << (5!=3) << endl;
+    cout << (3!=3) << endl;
+    
+
+    /*
+        Relational Operators:-
+        <
+        <=
+        >
+        >=
+        ==
+        !=
+
+        -Returns true(1) or false(0) only.
+    */
+    return 0;
+}
