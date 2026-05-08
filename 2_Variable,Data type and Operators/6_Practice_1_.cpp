5c744c668727ef55e7e475a06c878fe0096d2ffb
Niyati-Belani <niyatibelani1020@gmail.com>
2026-05-08 18:51:47 +0530

    Add New File

---
+++ 2_Variable,Data type and Operators/6_Practice_1_.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "sum of Two numbers." << endl;
+
+    int a,b;
+    int sum;
+
+    cout << "Enter Two Numbers: ";
+    cin >> a >> b;
+
+    sum = a + b;
+
+    cout << "Sum of Two number is:" << sum << endl;
+    
+    return 0;
+}
