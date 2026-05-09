be205fadaf735ce6e332efc8ed8b7bda35d3805c
Niyati-Belani <niyatibelani1020@gmail.com>
2026-05-09 15:20:25 +0530

    New File.

---
+++ 3_Conditional Statement , Loop_Nesting/2_Loops/5_Nested.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+        Nested loop:-
+        -Loop inside Loop.
+    */
+
+   for (int i = 1 ; i <= 5 ; i++)
+   {
+    for (int j = 1 ; j <= 5 ; j++)
+    {
+        cout << "*"  << " ";
+    }
+    cout << endl;
+   }
+   cout << endl;
+
+/*-------------------------------------------------------------------------*/
+
+    for (int i = 1 ; i <= 5 ; i++)
+   {
+    for (int j = 1 ; j <= 5 - i + 1 ; j++)
+    {
+        cout << "*"  << " ";
+    }
+    cout << endl;
+   }
+    return 0;
+}
