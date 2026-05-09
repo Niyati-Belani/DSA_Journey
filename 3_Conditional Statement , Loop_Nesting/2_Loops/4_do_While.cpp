fe184528761bba9660620f021a3adf72277690ed
Niyati-Belani <niyatibelani1020@gmail.com>
2026-05-09 14:40:07 +0530

    New file.

---
+++ 3_Conditional Statement , Loop_Nesting/2_Loops/4_do_While.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    do {
+        cout << "Niyati DSA Journey!" << endl;
+        i++;
+    } while (i <= 0);
+    cout << endl;
+
+/*-------------------------------------------------------------------------*/
+    
+int n = 0;
+    do{
+        cout << n << endl;
+        n++;
+    } while (n <=10);
+    cout << endl;
+
+    return 0;
+}
