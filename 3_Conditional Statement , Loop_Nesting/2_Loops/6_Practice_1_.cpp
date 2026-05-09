cd8461621cc9f33294f2f4d517c501316b71baf3
Niyati-Belani <niyatibelani1020@gmail.com>
2026-05-09 14:51:44 +0530

    New file

---
+++ 3_Conditional Statement , Loop_Nesting/2_Loops/6_Practice_1_.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    bool isPrime = true;
+
+    cout << "Enter number: " << endl;
+    cin >> n;
+
+    for ( int  i = 2 ; i <= n-1 ; i++)
+    {
+        if (n % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+
+   if (isPrime == true){
+        cout << "Prime!" << endl;
+   } else {
+        cout << "Not Prime!" << endl;
+   }
+
+   
+    return 0;
+}
