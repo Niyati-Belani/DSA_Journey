b99343e357fe49ae0635ddd6b867ad854927dd3f
Niyati-Belani <niyatibelani1020@gmail.com>
2026-05-09 18:31:17 +0530

    Modified & Rename File.

--- 4_Patterns/pattern_12.cpp
+++ 4_Patterns/pattern_12.cpp
@@ -1,33 +1,14 @@
 
 //METHOD-1
 
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    for (int row = 1; row <= 5 ; row++)
-    {
-        for (int col = 1; col <= 5-row+1; col++)
-        {
-            cout<<"* ";
-        }
-        
-        cout<<endl;
-    }
-    
-}
-
-//METHOD-2
-
 // #include<iostream>
 // using namespace std;
 
 // int main()
 // {
-//     for (int row = 5; row >= 1 ; row--)
+//     for (int row = 1; row <= 5 ; row++)
 //     {
-//         for (int col = 1; col <= row; col++)
+//         for (int col = 1; col <= 5-row+1; col++)
 //         {
 //             cout<<"* ";
 //         }
@@ -36,3 +17,22 @@ int main()
 //     }
     
 // }
+
+//METHOD-2
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for (int row = 5; row >= 1 ; row--)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            cout<<"* ";
+        }
+        
+        cout<<endl;
+    }
+    
+}
