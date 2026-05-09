3dc0648f53295113fd7c34225d407b8e1afcd458
Niyati-Belani <niyatibelani1020@gmail.com>
2026-05-09 14:32:03 +0530

    New Practice file

---
+++ 3_Conditional Statement , Loop_Nesting/2_Loops/6_Practice.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 20;
+    int oddSum = 0;
+
+    for (int i = 1 ; i <= n ; i++)
+    {
+        if (i % 2 != 0)
+        {
+            cout << i << " ";
+            oddSum += i;
+        }
+    }
+    cout << endl;
+    cout << oddSum;
+    return 0;
+}
