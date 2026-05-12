2b9b51f0780b1cfea06ed539501d6ebc4e49b8d9
Niyati-Belani <niyatibelani1020@gmail.com>
2026-05-12 19:47:01 +0530

    New File

---
+++ 4_Patterns/pattern_23.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1 ; i <= 5 ; i++)
+    {
+        for (int j = 1 ; j <= i ; j++)
+        {
+            cout << "*" << " ";
+        }
+        for (int k = 1 ; k <= 10-2*i ; k++)
+        {
+            cout << "  ";
+        }
+        for (int l = 1 ; l <= i ; l++)
+        {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 4 ; i >= 1 ; i--)
+    {
+        for (int j = 1 ; j <= i ; j++)
+        {
+            cout << "*" << " ";
+        }
+        for (int k = 1 ; k <= 10-2*i ; k++)
+        {
+            cout << "  ";
+        }
+        for (int l = 1 ; l <= i ; l++)
+        {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+}
