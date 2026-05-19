732b61f567eff832785cf275839d17afa2bf1209
Niyati-Belani <niyatibelani1020@gmail.com>
2026-05-19 22:46:06 +0530

    Modified file

--- 6_Binary Number system/2_binToDec.cpp
+++ 6_Binary Number system/2_binToDec.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 using namespace std;
 
+int bintoDec(int n)
+{
+    int ans = 0, pow = 1;
+
+    while (n > 0)
+    {
+        int rem = n % 10;
+        ans += rem * pow;
+
+        n /= 10;
+        pow *= 2;
+    }
+    return ans;
+}
 int main()
 {
     int binNum = 110010;
-    int ans = 0;
-    int poe = 1;
+
+    cout << "Binary to Decimal: " << bintoDec(binNum);
     return 0;
 }
 
