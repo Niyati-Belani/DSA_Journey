dbcbd272c06c8d5c892eab2958270828e1aeacc9
Niyati-Belani <niyatibelani1020@gmail.com>
2026-06-25 22:56:55 +0530

    Feat : Create Maximum Subarray Sum .

---
+++ 10_Pair Sum_Majority Element/2_maximumSubarraysum.cpp
@@ -0,0 +1,29 @@
+/*
+    Maximum subRRAY SUM:-
+    -Brute Force Approach
+*/
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    int arr[5] = {1,2,3,4,5};
+
+    int maxSum = INT_MIN;
+
+    for (int st = 0; st < n; st++)
+    {
+        int currSum = 0;
+        for (int end = st; end < n; end++)
+        {
+            currSum += arr[end];
+            maxSum = max(currSum , maxSum);
+        }
+    }
+
+    cout << "Max Subarray sum: " << maxSum << endl;
+    return 0;
+}
