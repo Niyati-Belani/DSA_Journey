204406684b3e122cd7ad7855ec360f54e846529e
Niyati-Belani <niyatibelani1020@gmail.com>
2026-06-25 23:34:54 +0530

    Feat : Create Kadane's Algorithm.

---
+++ 10_Subarray Kadane's Algorithm/3_kadanesAlgo.cpp
@@ -0,0 +1,33 @@
+/*
+    Kadane's Algorithm:
+    -most optimised
+*/
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+     int n = 5;
+    int arr[5] = {1,2,3,4,5};
+
+    int maxSum = INT_MIN;
+
+    for (int st = 0; st < n; st++)
+    {
+        int currSum = 0;
+        for (int end = st; end < n; end++)
+        {
+            currSum += arr[end];
+            maxSum = max(currSum , maxSum);
+            if(currSum < 0)
+            {
+                currSum = 0;
+            }
+        }
+    }
+
+    cout << "Max Subarray sum: " << maxSum << endl;
+    return 0;
+}
