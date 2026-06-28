1be775135625071a394486d7eec5a1a15ce0d041
Niyati-Belani <niyatibelani1020@gmail.com>
2026-06-28 17:13:48 +0530

    docs : Update code part.

--- 11_Pair Sum Majority ELement/1_pairSum.cpp
+++ 11_Pair Sum Majority ELement/1_pairSum.cpp
@@ -7,7 +7,33 @@
 #include <vector>
 using namespace std;
 
+vector<int> pairSum(vector<int> &nums, int target)
+{
+    vector<int> ans;
+    int n = nums.size();
+
+    int i = 0, j = n-1;
+
+    while(i>j)
+    {
+        int pairSum = nums[i] + nums[j];
+        if(pairSum > target)
+        {
+            j--;
+        } else if(pairSum < target)
+        {
+            i++;
+        } else
+        {
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);
+            return ans;
+        }
+    }
+    return ans;
+}
 int main()
 {
-    
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 13;
 }
