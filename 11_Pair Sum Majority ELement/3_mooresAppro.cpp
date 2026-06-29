f904273f7662845307b5c7549453a65ac1323fd0
Niyati-Belani <niyatibelani1020@gmail.com>
2026-06-29 21:41:29 +0530

    Refactor : Change return part in moores Algorithm.

--- 11_Pair Sum Majority ELement/3_mooresAppro.cpp
+++ 11_Pair Sum Majority ELement/3_mooresAppro.cpp
@@ -19,6 +19,24 @@ int majorityElement(vector<int>& nums)
             freq--;
         }
     }
+
+    int count = 0;
+    for (int val : nums)
+    {
+        if (val == ans)
+        {
+            count++;
+        }
+    }
+
+    if (count > nums.size()/2)
+    {
+        return ans;
+    }
+    else
+    {
+        return -1;
+    }
     return ans;
 }
 
