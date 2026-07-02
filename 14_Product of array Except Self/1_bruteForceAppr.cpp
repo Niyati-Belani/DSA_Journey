5aa1d473e830edbd89ebfef2706edc5ddcefcf20
Niyati-Belani <niyatibelani1020@gmail.com>
2026-07-02 21:24:59 +0530

    Refactor : Improve Code Structure.

--- 14_Product of array Except Self/1_bruteForceAppr.cpp
+++ 14_Product of array Except Self/1_bruteForceAppr.cpp
@@ -7,19 +7,17 @@ int main()
     vector<int> nums = {1,2,3,4};
     int n = nums.size();
 
-    vector<int> ans(n);
+    vector<int> ans(n, 1);
 
     for(int i = 0; i <n; i++)
     {
-        int product = 1;
         for(int j = 0; j < n; j++)
         {
             if(j != i)
             {
-                product *= nums[j];
+                ans[i] *= nums[j];
             }
         }
-        ans[i]=product;
     }
     cout << "Product of array except self is: ";
     for(int i = 0; i < n; i++)
