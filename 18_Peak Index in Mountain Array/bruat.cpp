418abeca2e5c4307420d63dfd7af9d0143aecf58
Niyati-Belani <niyatibelani1020@gmail.com>
2026-07-13 22:30:08 +0530

    Feat : Create Peak Index in Mountain Array.

---
+++ 18_Peak Index in Mountain Array/bruat.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int peak(vector<int>& arr, int st, int end)
+{
+    int mid = st + (end - st )/2;
+    if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid+1])
+    {
+        return mid;
+    }
+    else if (arr[mid] < arr[mid + 1])
+    {
+        return peak(arr, mid +1 , end);
+    }
+    else
+    {
+        return peak(arr, st, mid -1);
+    }
+}
+int main()
+{
+    vector<int> arr = {0,3,8,9,5,2};
+
+    cout << peak (arr, 0, arr.size()-1) << endl;
+    return 0;
+}
